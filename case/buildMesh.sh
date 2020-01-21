4c44462a4f81f6257a4e4635c0cf98dea72b4e19
Axel Fiedler <fiedler.hh@gmail.com>
2020-01-21 17:20:44 +0100

    Simplified mesh creation procedure

--- case/buildMesh.sh
+++ case/buildMesh.sh
@@ -9,11 +9,7 @@ rm log*
 rm -r 0.* 1* 2* 3* 4* 5* 6* 7* 8* 9*
 rm -r processor*
 rm -r constant/polyMesh
-rm constant/triSurface/suzanne_scaled.stl
-rm constant/triSurface/ears_scaled.stl
 blockMesh >> logBlockMesh
-surfaceConvert constant/triSurface/suzanne.stl constant/triSurface/suzanne_scaled.stl -clean -scale 0.1 >> logSurfaceConvert
-surfaceConvert constant/triSurface/ears.stl constant/triSurface/ears_scaled.stl -clean -scale 0.1 >> logSurfaceConvert
 snappyHexMesh -overwrite >> logSnappyHexMesh
 #decomposePar >> logSnappyHexMesh
 #mpirun -np 4 snappyHexMesh -overwrite -parallel >> logSnappyHexMesh
