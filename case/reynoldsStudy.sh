405492713e8994cc7b210e10c18d22a0617d64b2
Axel Fiedler <fiedler.hh@gmail.com>
2020-01-21 17:36:41 +0100

    Added possibility to run turbulent calculations using kEpsilon or kOmega.

--- case/reynoldsStudy.sh
+++ case/reynoldsStudy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Contributor: Axel Fiedler
-# Updated on: 01.07.2018
+# Updated on: 20.01.2020
 
 echo -e "This script will run a Reynolds number study of a flow around Suzanne"
 
-for Re in {100..2000..100}
+for Re in {1000..8000..1000}
 do
 	echo -e "Re: $Re"
 
@@ -13,12 +13,33 @@ do
 	rm -r processor*
 	rm -r 0.* 1* 2* 3* 4* 5* 6* 7* 8* 9*
 	rm log*
+	rm 0/cellLevel 0/pointLevel
+	rm -r postProcessing/forces postProcessing/residuals
 
 	echo -e "\nSet magUInf.\n"
-	sed -i "79s/.*/        magUInf     $(python -c "Re=$Re; nu=0.00001; Dhyd=4*0.02562 / 0.804201; U=Re*nu/Dhyd; print U;"); /" system/controlDict
+	sed -i "78s/.*/        magUInf     $(python -c "Re=$Re; nu=0.00001; Dhyd=4*0.02562 / 0.804201; U=Re*nu/Dhyd; print U;"); /" system/controlDict
 
 	echo -e "Set inlet velocity.\n"
 	sed -i "27s/.*/        value           uniform (0 $(python -c "Re=$Re; nu=0.00001; Dhyd=4*0.02562 / 0.804201; U=Re*nu/Dhyd; print U;") 0); /" 0/U # Dhyd = 4*A/U; U = Re*nu/Dhyd;
+
+	# Uncomment following sections for k-epsilon or k-Omega calculation and set constant/turbelenceProperties accordingly
+
+	echo -e "Set turbulent kinetic energy at inlet.\n"
+	sed -i "19s/.*/internalField   uniform $(python -c "I=0.05; Re=$Re; nu=0.00001; Dhyd=4*0.02562 / 0.804201; U=Re*nu/Dhyd; k = 1.5*(U*I)**2; print k;"); /" 0/k # k = 1.5*(U*I_T)^2
+	sed -i "26s/.*/        value           uniform $(python -c "I=0.05; Re=$Re; nu=0.00001; Dhyd=4*0.02562 / 0.804201; U=Re*nu/Dhyd; k = 1.5*(U*I)**2; print k;"); /" 0/k # k = 1.5*(U*I_T)^2
+	sed -i "37s/.*/        value           uniform $(python -c "I=0.05; Re=$Re; nu=0.00001; Dhyd=4*0.02562 / 0.804201; U=Re*nu/Dhyd; k = 1.5*(U*I)**2; print k;"); /" 0/k # k = 1.5*(U*I_T)^2
+
+	echo -e "Set turbulent dissipation at inlet.\n"
+
+	sed -i "20s/.*/internalField           uniform $(python -c "I=0.05; Re=$Re; nu=0.00001; Dhyd=4*0.02562 / 0.804201; U=Re*nu/Dhyd; k = 1.5*(U*I)**2; epsilon = 0.09**(3/4) * k**(3/2)/(0.07*Dhyd); print epsilon;"); /" 0/epsilon # epsilon = 0.09^(3/4) * k^(3/2)/(0.07*L_char); omega = epsilon/k
+	sed -i "27s/.*/        value      		uniform $(python -c "I=0.05; Re=$Re; nu=0.00001; Dhyd=4*0.02562 / 0.804201; U=Re*nu/Dhyd; k = 1.5*(U*I)**2; epsilon = 0.09**(3/4) * k**(3/2)/(0.07*Dhyd); print epsilon;"); /" 0/epsilon # epsilon = 0.09^(3/4) * k^(3/2)/(0.07*L_char); omega = epsilon/k
+	sed -i "38s/.*/        value      		uniform $(python -c "I=0.05; Re=$Re; nu=0.00001; Dhyd=4*0.02562 / 0.804201; U=Re*nu/Dhyd; k = 1.5*(U*I)**2; epsilon = 0.09**(3/4) * k**(3/2)/(0.07*Dhyd); print epsilon;"); /" 0/epsilon # epsilon = 0.09^(3/4) * k^(3/2)/(0.07*L_char); omega = epsilon/k
+
+	echo -e "Set specific turbulent dissipation at inlet.\n"
+
+	sed -i "19s/.*/internalField           uniform $(python -c "I=0.05; Re=$Re; nu=0.00001; Dhyd=4*0.02562 / 0.804201; U=Re*nu/Dhyd; k = 1.5*(U*I)**2; epsilon = 0.09**(3/4) * k**(3/2)/(0.07*Dhyd); omega=epsilon/k; print omega;"); /" 0/omega # epsilon = 0.09^(3/4) * k^(3/2)/(0.07*L_char); omega = epsilon/k
+	sed -i "26s/.*/        value      		uniform $(python -c "I=0.05; Re=$Re; nu=0.00001; Dhyd=4*0.02562 / 0.804201; U=Re*nu/Dhyd; k = 1.5*(U*I)**2; epsilon = 0.09**(3/4) * k**(3/2)/(0.07*Dhyd); omega=epsilon/k; print omega;"); /" 0/omega # epsilon = 0.09^(3/4) * k^(3/2)/(0.07*L_char); omega = epsilon/k
+	sed -i "37s/.*/        value      		uniform $(python -c "I=0.05; Re=$Re; nu=0.00001; Dhyd=4*0.02562 / 0.804201; U=Re*nu/Dhyd; k = 1.5*(U*I)**2; epsilon = 0.09**(3/4) * k**(3/2)/(0.07*Dhyd); omega=epsilon/k; print omega;"); /" 0/omega # epsilon = 0.09^(3/4) * k^(3/2)/(0.07*L_char); omega = epsilon/k
 
 	echo -e "Decompose case.\n"
 	decomposePar >> logSimpleFoam
